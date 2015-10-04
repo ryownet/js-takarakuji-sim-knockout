ee53b6ccd476e15b232d25fc941d5cdb1c57281e
ryownet <ryownet@gmail.com>
2015-10-04 22:01:09 +0900

    knockout added

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,8 @@
 
   var $ = w.jQuery;
   var kujiUtil = require('./_kujiUtil');
-  var dispResult = require('./_dispResult');
   var judge = require('./_judge')
+  var ko = window.ko || {};
 
   //速度計測用. 10秒間のくじ判定数をアラート
   var isDebug = false;
@@ -14,60 +14,67 @@
 /*
   くじ結果データ管理のviewModel
  */
-var vm = new Vue({
-  el: '#app',
-  data: {
-    totalSpendMoney: 0,
-    totalGetMoney: 0,
-    totalKujiCount: 0,
-    sagaku: 0,
-    dispItems: []
-  },
-  computed: {
-    isMinus: function () {
-      return this.sagaku < 0 ;
-    }
-  }
-});
-Vue.filter('kanma', function (value) {
-  return value.toLocaleString();
-})
+
+var vm = {
+  totalSpendMoney: ko.observable(0),
+  totalGetMoney: ko.observable(0),
+  totalKujiCount: ko.observable(0),
+  items: []
+};
+vm.sagaku = ko.computed(function() {
+  var str = vm.totalGetMoney() - vm.totalSpendMoney() || 0;
+  return str.toLocaleString()
+}, this);
+vm.sagakuIsMinus = function(){
+  return vm.totalGetMoney() - vm.totalSpendMoney() || 0;
+}
+vm.dispItems = ko.observableArray(vm.items);
+
+
 
 /*
 スタートストップボタン管理のviewModel
 */
-var actionVm = new Vue({
-  el: '#actionBar',
-  data: {
-    isStop: true,
-    isStart: false
-  },
-  methods: {
-    start: function (e) {
-      this.isStop = false;
-      this.isStart = true;
-      kujiUtil.intervalID = setInterval( this.getKuji, kujiUtil.INTERVAL );
-      if(isDebug){
-        setTimeout(function (){
-          clearInterval( kujiUtil.intervalID );
-          this.isStop = true;
-          window.alert(vm.totalKujiCount);
-        }, 10000);
-      }
-    },
-    stop: function (e) {
-      this.isStop = true;
-      this.isStart = false;
-      clearInterval( kujiUtil.intervalID );
-    },
-    getKuji : function (){
-      var k = new kujiUtil.Kuji();
-      var j = judge(k, kujiUtil);
-      dispResult(j, vm, kujiUtil);
+var actionVm = {
+  isStop: ko.observable(true),
+  start: function (e) {
+    this.isStop(false);
+    kujiUtil.intervalID = setInterval( this.getKuji, kujiUtil.INTERVAL );
+    if(isDebug){
+      setTimeout(function (){
+        clearInterval( kujiUtil.intervalID );
+        actionVm.isStop(true);
+        window.alert(vm.totalKujiCount());
+      }, 10000);
     }
+  },
+  stop: function (e) {
+    this.isStop(true);
+    clearInterval( kujiUtil.intervalID );
+  },
+  getKuji : function (){
+    var k = new kujiUtil.Kuji();
+    var j = judge(k, kujiUtil);
+    actionVm.dispResult(j);
+  },
+  dispResult : function (atariKuji) {
+    if( atariKuji.name ){
+      // console.time('t')
+      vm.totalGetMoney(vm.totalGetMoney() + atariKuji.kingaku);
+      vm.items.forEach(function(itm, idx){
+        if (itm.category === atariKuji.category){
+          itm.atariCount(itm.atariCount()+1)
+        }
+      });
+      //console.timeEnd('t')
+    }
+    vm.totalKujiCount(vm.totalKujiCount()+1);
+    vm.totalSpendMoney(vm.totalSpendMoney() +  kujiUtil.TANKA);
   }
-});
-
+}
+actionVm.isStart= ko.computed(function (){
+  return !actionVm.isStop()
+}, this);
 
 
 
@@ -80,7 +87,6 @@ $(document).ready(function (){
 
     var l = kujiUtil.atariArr.length;
     var category = '';
-    var items = [];
     for(var i=0; i< l; i++){
       if(kujiUtil.atariArr[i].category === category) continue;
       category = kujiUtil.atariArr[i].category;
@@ -88,13 +94,15 @@ $(document).ready(function (){
         name: kujiUtil.atariArr[i].name,
         kingaku: kujiUtil.atariArr[i].kingaku,
         category: kujiUtil.atariArr[i].category,
-        atariCount: 0
+        atariCount: ko.observable(0)
       };
-      vm.dispItems.push( atariVmData );
+      vm.items.push( atariVmData );
     }
+    // knockout start!
+    ko.applyBindings(vm, document.getElementById('app'));
+    ko.applyBindings(actionVm, document.getElementById('actionBar'));
   })
 });
-
 
 
 
